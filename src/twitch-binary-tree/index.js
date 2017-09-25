9b7fda0cc4a94833e1c797245d91aaa24b27341a
Oliver Benns <oliverbenns@gmail.com>
2017-09-25 12:24:33 +0100

    Add twitch binary tree question

---
+++ src/twitch-binary-tree/index.js
@@ -0,0 +1,8 @@
+// Twitch interview question
+// For a given binary tree, assign the sibling pointer of each node.
+// A sibling is always the node to its immediate right on the same level of the tree.
+
+//        5
+//    6       2
+//  8   3   4   7
+const tree = require('./tree');
