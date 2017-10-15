bc9527948dd1491088f96998ac054e9525de8cb5
Oliver Benns <oliverbenns@gmail.com>
2017-10-15 12:32:58 +0100

    Add initial student records commit

---
+++ src/parse-student-records/index.js
@@ -0,0 +1,16 @@
+// Read the files
+
+// For each file
+  // split to lines
+
+// For each line
+  // Split by delimiter.
+
+  // For each entry into line
+    // Map to object.
+
+
+
+// 3 times
+  // With every record
+    // sort by
