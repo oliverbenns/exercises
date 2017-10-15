356c81a409d03d4ec77bee1c9930ab83d5a9f5ef
Oliver Benns <oliverbenns@gmail.com>
2017-10-15 13:54:51 +0100

    Add reading and parsing of file contents

--- src/parse-student-records/index.js
+++ src/parse-student-records/index.js
@@ -1,16 +1,65 @@
-// Read the files
+const readFile = require('./read-file');
 
-// For each file
-  // split to lines
+const files = [
+  {
+    name: 'comma.txt',
+    delimiter: ',',
+    keys: ['last_name', 'first_name', 'campus', 'favorite_color', 'date_of_birth'],
+    data: null,
+  },
+  {
+    name: 'dollar.txt',
+    delimiter: '$',
+    keys: ['last_name', 'first_name', 'middle_initial', 'campus', 'date_of_birth', 'favorite_color'],
+    data: null,
+  },
+  {
+    name: 'pipe.txt',
+    delimiter: '|',
+    keys: ['last_name', 'first_name', 'middle_initial', 'campus', 'favorite_color', 'date_of_birth'],
+    data: null,
+  },
+];
 
-// For each line
-  // Split by delimiter.
+const promises = files.map(f => readFile(f.name));
 
-  // For each entry into line
-    // Map to object.
+const parseData = file => {
+  const rows = file.data
+    .split('\n')
+    .filter(row => row !== '') // My editor adds new lines to EOF.
+    .map(row => {
+      return row
+        .split(file.delimiter)
+        .map(c => c.trim()); // Removes whitespace from either side of delimiter.
+    });
 
+  const record = {};
 
+  const records = rows.map(row => {
+    const record = {};
 
-// 3 times
-  // With every record
-    // sort by
+    // Loop through columns and map to ordering.
+    row.forEach((column, index) => {
+      const key = file.keys[index];
+      record[key] = column;
+    })
+
+    return record;
+  })
+
+  return records;
+};
+
+Promise
+  .all(promises)
+  .then(data => data.map((d, i) => {
+    const file = files[i];
+    file.data = d;
+
+    return file;
+  }))
+  .then(files => files.map(parseData))
+  .then(parsedFiles => [].concat(...parsedFiles)) // Flatten all of our files to single array.
+  .then(records => {
+    console.log('records', records);
+  });
---
+++ src/parse-student-records/read-file.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+// Node's FS readFile as a promise.
+const readFile = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(`${__dirname}/data/${file}`, 'utf8', (err, data) => err ? reject(err) : resolve(data));
+  });
+};
+
+
+module.exports = readFile;
