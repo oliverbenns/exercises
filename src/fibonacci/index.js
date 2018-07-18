ecfba1afe856edb8e084fce4da0e7ff6f8bd651c
Oliver Benns <oliverbenns@gmail.com>
2018-07-18 22:42:45 +0200

    Add fibonacci

---
+++ src/fibonacci/index.js
@@ -0,0 +1,7 @@
+// Write a function to return an n element in Fibonacci sequence”
+
+const fibonacci = n => {
+  return 1;
+};
+
+module.exports = fibonacci;
---
+++ src/fibonacci/test.js
@@ -0,0 +1,16 @@
+const fibonnaci = require('.');
+const test = require('ava');
+
+const cases = [
+  { n: 1, expected: 1 },
+  { n: 5, expected: 5 },
+  { n: 9, expected: 34 },
+  { n: 12, expected: 144 },
+];
+
+cases.forEach(c => {
+  test(`returns ${c.expected} when n is ${c.n}`, t => {
+    const result = fibonnaci(c.n);
+    t.is(result, c.expected);
+  });
+});
